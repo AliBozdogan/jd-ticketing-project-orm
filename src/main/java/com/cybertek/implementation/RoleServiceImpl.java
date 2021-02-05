03cd0d21224cce2f6543cad5ba02b7f20597af1c
Ali Bozdogan <ali.h.bozdogan@gmail.com>
2021-02-04 19:49:48 -0600

    User impl Update Portion

--- src/main/java/com/cybertek/implementation/RoleServiceImpl.java
+++ src/main/java/com/cybertek/implementation/RoleServiceImpl.java
@@ -1,20 +1,39 @@
 package com.cybertek.implementation;
 
+import com.cybertek.Repository.RoleRepository;
 import com.cybertek.dto.RoleDTO;
+import com.cybertek.entity.Role;
+import com.cybertek.mapper.RoleMapper;
 import com.cybertek.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
-    @Override
-    public List<RoleDTO> listAllRoles() {
-        return null;
+
+    private RoleRepository roleRepository;
+    private RoleMapper roleMapper;
+
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
-    public RoleDTO findById(long id) {
-        return null;
+    public List<RoleDTO> listAllRoles() {
+        List<Role> list = roleRepository.findAll();
+
+
+        return list.stream().map(obj -> {return roleMapper.convertToDto(obj);}).collect(Collectors.toList());
+    }
+
+    @Override
+    public RoleDTO findById(Long id) {
+        Role role = roleRepository.findById(id).get();
+
+        return roleMapper.convertToDto(role);
     }
 }
